35302413365999acea608b6b6a6cdfc6a6419072
philschmid <schmidphilipp1995@gmail.com>
2021-03-08 16:22:44 +0100

    added push to example

--- samples/docker/run_glue/scripts/train-cloud.sh
+++ samples/docker/run_glue/scripts/train-cloud.sh
@@ -17,38 +17,38 @@ IMAGE_URI=gcr.io/${PROJECT_ID}/${IMAGE_REPO_NAME}:${IMAGE_TAG} # IMAGE_URI: the 
 docker build -f Dockerfile -t ${IMAGE_URI} ./
 
 # Deploy the docker image to Cloud Container Registry
-# docker push ${IMAGE_URI}
+docker push ${IMAGE_URI}
 
-# # Submit your training job
-# echo "Submitting the training job"
+# Submit your training job
+echo "Submitting the training job"
 
-# # These variables are passed to the docker image
-# JOB_DIR=gs://${BUCKET_NAME}/${JOB_NAME}/models
-
-
-# gcloud ai-platform jobs submit training ${JOB_NAME} \
-#     --region ${REGION} \
-#     --master-image-uri ${IMAGE_URI} \
-#     --job-dir ${JOB_DIR} \
-#     --scale-tier custom \
-#     --master-machine-type ${MACHINE_TYPE} \
-#     --worker-machine-type ${MACHINE_TYPE} \
-#     --worker-count 1 \
-#     -- \
-#     --model_name_or_path bert-base-cased \
-#     --task_name mrpc \
-#     --do_train True \
-#     --do_eval True \
-#     --max_seq_length 128 \
-#     --per_device_train_batch_size 32 \
-#     --learning_rate 2e-5 \
-#     --num_train_epochs 3 \
-#     --output_dir /tmp/mrpc/
+# These variables are passed to the docker image
+JOB_DIR=gs://${BUCKET_NAME}/${JOB_NAME}/models
 
 
-# # Stream the logs from the job
-# gcloud ai-platform jobs stream-logs ${JOB_NAME}
+gcloud ai-platform jobs submit training ${JOB_NAME} \
+    --region ${REGION} \
+    --master-image-uri ${IMAGE_URI} \
+    --job-dir ${JOB_DIR} \
+    --scale-tier custom \
+    --master-machine-type ${MACHINE_TYPE} \
+    --worker-machine-type ${MACHINE_TYPE} \
+    --worker-count 1 \
+    -- \
+    --model_name_or_path bert-base-cased \
+    --task_name mrpc \
+    --do_train True \
+    --do_eval True \
+    --max_seq_length 128 \
+    --per_device_train_batch_size 32 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3 \
+    --output_dir /tmp/mrpc/
 
-# # Verify the model was exported
-# echo "Verify the model was exported:"
-# gsutil ls ${JOB_DIR}/model_*
+
+# Stream the logs from the job
+gcloud ai-platform jobs stream-logs ${JOB_NAME}
+
+# Verify the model was exported
+echo "Verify the model was exported:"
+gsutil ls ${JOB_DIR}/model_*
